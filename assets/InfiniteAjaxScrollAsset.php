26d322bcb38aec8b1107ca7bba20b024c2a8c8fa
Ivan Koptiev <ikoptev@gmail.com>
2014-01-09 04:25:51 +0200

    Documentation updates

--- assets/InfiniteAjaxScrollAsset.php
+++ assets/InfiniteAjaxScrollAsset.php
@@ -7,11 +7,9 @@ use yii\web\AssetBundle;
 /**
  * This is "InfiniteAjaxScrollAsset" class.
  *
- * This class is a Yii2 asset bundle for JQuery Infinite Ajax Scroll plugin.
+ * This class is an asset bundle for {@link https://github.com/webcreate/infinite-ajax-scroll JQuery Infinite Ajax Scroll plugin}.
  *
  * @link      http://kop.github.io/yii2-scroll-pager Y2SP project page.
- * @link      https://github.com/webcreate/infinite-ajax-scroll JQuery IAS project page.
- *
  * @license   https://github.com/kop/yii2-scroll-pager/blob/master/LICENSE.md MIT
  *
  * @author    Ivan Koptiev <ikoptev@gmail.com>
