27720921e60b5df12b73c31429472fe8781e0425
Alexandr Kozhevnikov <onmotion1@gmail.com>
2020-07-08 10:52:26 +0300

    fix #106

--- messages/en/general.php
+++ messages/en/general.php
@@ -2,6 +2,6 @@
 
 return [
     'Load more items' => 'Load more items',
-    'You reached the end' => 'You reached the end'
+    'You reached the end' => 'You reached the end',
     'Load previous items' => 'Load previous items'
 ];
